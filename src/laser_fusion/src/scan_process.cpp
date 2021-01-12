5b8a65c254e3c4a2fa1bd21380300f74a9f6e096
xiaobrnbrn <2715945715@qq.com>
2021-01-12 23:07:05 +0800

    commit

--- src/laser_fusion/src/scan_process.cpp
+++ src/laser_fusion/src/scan_process.cpp
@@ -22,11 +22,11 @@
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/kdtree/kdtree_flann.h>
 
-#include "common.h"
+#include </home/sishubin/SLAMcodes/My_Local_Repo/Laser_fusion/src/laser_fusion/include/laser_fusion/common.h>
 
 
 using namespace std;
-using namespace ros;
+//using namespace ros;
 
 /****雷达的基本参数设置****/
 //点云扫描周期,一般10Hz
@@ -50,7 +50,6 @@ void laserCloudHandler(const sensor_msgs::PointCloud2::ConstPtr& laserCloudmsg)
         }
     return;
     }
-
 
     //记录每个scan有曲率的点的开始和结束索引
     std::vector<int> scanStartInd(N_SCANS,0);
@@ -87,9 +86,81 @@ void laserCloudHandler(const sensor_msgs::PointCloud2::ConstPtr& laserCloudmsg)
     int count = cloudSize;
     PointType point_;
     std::vector<pcl::PointCloud<PointType>> laserCloudScans(N_SCANS);
-    for(int i=0;i<count;i++)
+    for(int i=0;i<cloudSize;i++)
     {
+        //坐标轴交换，velodyne lidar的坐标系也转换到z轴向前，x轴向左的右手坐标系
+        point_.x = laserCloudIn.points[i].y;
+        point_.y = laserCloudIn.points[i].z;
+        point_.z = laserCloudIn.points[i].x;
+
+        //计算点的仰角,然后根据仰角排列激光号,velodyne/robosense每两个scan之间间隔是2度(垂直方向)
+        //仰角的计算公式: atan(y/sqrt(x^2+z^2)),单位:角度
+        float angle = atan2(point_.y , sqrt(point_.x*point_.x + point_.z*point_.z)) * 180 / M_PI;
+        int scanID;
+        //对仰角取整,四舍五入
+        int roundedAngle = int(angle+(angle<0.0? -0.5:+0.5));
+        //记录点所在的scanID, VLD-16/robosense中ID范围:0~15,共16线
+        // VLD-16/robosense中仰角从15度->-15度时,ID从15递减至0
+        if(roundedAngle > 0)
+        {
+            scanID = roundedAngle;
+        }
+        else
+        {
+            scanID = roundedAngle + (N_SCANS - 1);
+        }
+        //过滤掉scanID不正确的点
+        if (scanID > (N_SCANS - 1) || scanID < 0 )
+        {
+            count--;
+            continue;
+         }
+        //计算该点的旋转角,旋转角定义为空间中的点与z轴方向的夹角
+        float ori = -atan2(point_.x,point_.z);
+        //?????????旋转角的修正部分没看懂,loam中旋转角是如何定义的??
+        if(!halfPassed)
+        {
+            //扫描未过半
+            //根据扫描线是否旋转过半选择与起始位置还是终止位置进行差值计算，从而进行补偿
+            //确保-pi/2 < ori - startOri < 3*pi/2
+            //??????为啥不是0< ori - startOri < 2*pi
+            if(ori < startOri - M_PI/2)
+            {
+                ori += 2*M_PI;
+            }
+            else if(ori > startOri + M_PI*3/2)
+            {
+                ori -= 2*M_PI;
+            }
+            //旋转超过半圈
+            if(ori - startOri > M_PI)
+            {
+                halfPassed = true;
+            }
+        }
+        else
+        {
+            //旋转超过半圈
+            //????????超过半圈后为啥要加2*pi
+            ori += 2 * M_PI;
+            //确保-3*pi/2 < ori - endOri < pi/2
+            if (ori < endOri - M_PI * 3 / 2) 
+            {
+                ori += 2 * M_PI;
+            } 
+            else if (ori > endOri + M_PI / 2)
+             {
+                ori -= 2 * M_PI;
+            }
+        }
         
+
+        
+
+
+
+
+
     }
 
 
@@ -117,7 +188,7 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "scan_process");
     ros::NodeHandle nh;
 
-    ros::Subscriber subLaserCloud = nh.subscribe<sensor_msgs::PointCloud2>("/points_raw",2,laserCloudHandler);
+    ros::Subscriber subLaserCloud = nh.subscribe<sensor_msgs::PointCloud2>("/rslidar_points",2,laserCloudHandler);
 
     
     return 0;
