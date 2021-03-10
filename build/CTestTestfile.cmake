91f3b606f0ef92088de20a15f8bc2dbe036e9fb9
xiaobrnbrn <2715945715@qq.com>
2021-03-10 10:15:21 +0800

    auto commit

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sishubin/SLAMcodes/My_Local_Repo/Laser_fusion/src
-# Build directory: /home/sishubin/SLAMcodes/My_Local_Repo/Laser_fusion/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("laser_fusion")
