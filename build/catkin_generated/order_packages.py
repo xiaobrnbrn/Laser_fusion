3fcb1d8b590fd3e59fc0d956d4b094d482f332ae
xiaobrnbrn <2715945715@qq.com>
2021-06-26 14:30:20 +0800

    auto commit

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/omnisky/aSLAMcodes/My_Local_Repo/Laser_fusion/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/omnisky/aSLAMcodes/sirius_ws/devel;/opt/ros/melodic".split(';') if "/home/omnisky/aSLAMcodes/sirius_ws/devel;/opt/ros/melodic" != "" else []
--- build/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /home/omnisky/aSLAMcodes/My_Local_Repo/Laser_fusion/build/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
